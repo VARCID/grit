9c0d32ee50abd2f9807215f68f3a615df7125c32
Robert-Schmid <robert.schmid@uni-konstanz.de>
2015-07-20 11:36:23 +0200

    added save PID to file in runscript

--- runscript.sh
+++ runscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 chmod +x bin/GRIT
-./bin/GRIT java -Xdebug -Xnoagent \ -Djava.compiler=NONE \ -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=6001 
+./bin/GRIT java -Xdebug -Xnoagent \ -Djava.compiler=NONE \ -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=6001 &
+echo $! > grit.pid
