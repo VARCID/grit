0c146ed0d7b28fff81e5a137f3334d1c5073edce
Robert-Schmid <robert.schmid@uni-konstanz.de>
2015-07-21 13:50:04 +0200

    grit starts now in background
    this fixes possible errors with shutdownGrit.sh
    output is now saved in log/grit.out an deleted at restart

--- runscript.sh
+++ runscript.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 chmod +x bin/GRIT
-./bin/GRIT java -Xdebug -Xnoagent \ -Djava.compiler=NONE \ -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=6001 &
+if [ -e ./log/grit.out ]
+ then
+  rm -f ./log/grit.out
+  echo "old grit.old deleted"
+fi
+nohup ./bin/GRIT java -Xdebug -Xnoagent \ -Djava.compiler=NONE \ -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=6001 &>./log/grit.out &
 echo $! > grit.pid
